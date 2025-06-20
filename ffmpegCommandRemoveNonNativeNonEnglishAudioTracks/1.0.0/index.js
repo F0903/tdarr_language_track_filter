a98001d5d5000149f4b605e35127aaf82de54211
Frederik P. <frepbh@gmail.com>
2025-06-21 00:04:43 +0200

    changes

--- ffmpegCommandRemoveNonNativeNonEnglishAudioTracks/1.0.0/index.js
+++ ffmpegCommandRemoveNonNativeNonEnglishAudioTracks/1.0.0/index.js
@@ -1,3 +1,22 @@
+"use strict";
+// TESTING
+var __assign =
+  (this && this.__assign) ||
+  function () {
+    __assign =
+      Object.assign ||
+      function (t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+      };
+    return __assign.apply(this, arguments);
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+
 const details = () => ({
   name: "Remove Non-Native Non-English Audio Tracks",
   description: `Removes audio tracks that are not in the native language or are not English.
@@ -10,7 +29,7 @@ const details = () => ({
   isStartPlgin: false,
   pType: "",
   requiresVersion: "2.11.01",
-  sidebarPosition: 1,
+  sidebarPosition: -1,
   icon: "",
   inputs: [
     {
@@ -30,6 +49,17 @@ const details = () => ({
       defaultValue: "",
       inputUI: {
         type: "text",
+        displayConditions: {
+          logic: "AND",
+          sets: {
+            logic: "AND",
+            inputs: {
+              name: "provider",
+              value: "Radarr",
+              condition: "===",
+            },
+          },
+        },
       },
       tooltip: "Input your Radarr api key here.",
     },
@@ -39,6 +69,17 @@ const details = () => ({
       defaultValue: "http://192.168.1.2:7878",
       inputUI: {
         type: "text",
+        displayConditions: {
+          logic: "AND",
+          sets: {
+            logic: "AND",
+            inputs: {
+              name: "provider",
+              value: "Radarr",
+              condition: "===",
+            },
+          },
+        },
       },
       tooltip:
         "Input your full Radarr url here." +
@@ -52,6 +93,17 @@ const details = () => ({
       defaultValue: "",
       inputUI: {
         type: "text",
+        displayConditions: {
+          logic: "AND",
+          sets: {
+            logic: "AND",
+            inputs: {
+              name: "provider",
+              value: "Sonarr",
+              condition: "===",
+            },
+          },
+        },
       },
       tooltip: "Input your Sonarr api key here.",
     },
@@ -61,6 +113,17 @@ const details = () => ({
       defaultValue: "http://192.168.1.2:8989",
       inputUI: {
         type: "text",
+        displayConditions: {
+          logic: "AND",
+          sets: {
+            logic: "AND",
+            inputs: {
+              name: "provider",
+              value: "Sonarr",
+              condition: "===",
+            },
+          },
+        },
       },
       tooltip:
         "Input your full Sonarr url here." +
