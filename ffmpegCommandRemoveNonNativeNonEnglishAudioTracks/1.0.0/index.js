1d40e2e1b4ded729de28b452b99fcea1d89337d3
Frederik P. <frepbh@gmail.com>
2025-06-22 01:38:58 +0200

    Apparently a "overallOuputArguments" is an intended misspelling

--- ffmpegCommandRemoveNonNativeNonEnglishAudioTracks/1.0.0/index.js
+++ ffmpegCommandRemoveNonNativeNonEnglishAudioTracks/1.0.0/index.js
@@ -216,7 +216,7 @@ const filterAudioTracks = (args, langsToKeep, nativeLanguage) => {
 
   if (nativeStream) {
     // Set the native language stream as default
-    args.variables.ffmpegCommand.overallOutputArguments.push(
+    args.variables.ffmpegCommand.overallOuputArguments.push(
       `-disposition:a:${nativeStream.index} 1`
     );
     args.jobLog(
